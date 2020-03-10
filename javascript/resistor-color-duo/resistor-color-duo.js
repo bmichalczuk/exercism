2238c487c6f960654a1e8101405567f0b622ad57
bmichalczuk <barmic90@hotmail.com>
2020-03-10 18:47:16 +0100

    submit resistor-color-duo, download react

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,8 +1,13 @@
 //
 // This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
+
 // convenience to get you started writing code faster.
 //
-
-export const decodedValue = () => {
-  throw new Error("Remove this statement and implement this function");
+export const decodedValue = (bandsColors) => {
+  const RESISTORS_COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
+  const firstTwoBandsCode = bandsColors
+    .slice(0,2)
+    .map(color => RESISTORS_COLORS.indexOf(color))
+    .join("");
+    return parseInt(firstTwoBandsCode);
 };
