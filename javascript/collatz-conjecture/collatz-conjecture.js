225da5669f9dafd2b1325fdd9a5b865fa34bbcf3
bmichalczuk <barmic90@hotmail.com>
2020-03-23 21:12:59 +0100

    submit hamming and collatz-conjecture

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -3,6 +3,24 @@
 // convenience to get you started writing code faster.
 //
 
-export const steps = () => {
-  throw new Error("Remove this statement and implement this function");
+
+export const steps = (n, stepsCount = 0) => {
+  if(n < 1) {
+    throw new Error('Only positive numbers are allowed');
+  }
+  if(n === 1) {
+    return stepsCount;
+  }
+  stepsCount++
+  return n % 2 === 0 ? steps(n/2, stepsCount) : steps(n * 3 + 1, stepsCount);
 };
+
+
+/*
+Take any positive integer n. If n is even, divide n by 2 to get n / 2. If n is
+odd, multiply n by 3 and add 1 to get 3n + 1. Repeat the process indefinitely.
+The conjecture states that no matter which number you start with, you will
+always reach 1 eventually.
+
+Given a number n, return the number of steps required to reach 1.
+*/
--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -3,6 +3,20 @@
 // convenience to get you started writing code faster.
 //
 
-export const compute = () => {
-  throw new Error("Remove this statement and implement this function");
+export const compute = (strandA, strandB) => {
+  
+  if(strandA === "" && strandB === "") {
+    return 0;
+  }
+  if(strandA === "") {
+    throw new Error("left strand must not be empty") ;
+  }
+  if(strandB === "") {
+    return new Error("right strand must not be empty");
+  }
+  if(strandA.length !== strandB.length) {
+    throw new Error("left and right strands must be of equal length");
+  }
+  return [...strandA].reduce((acc, curr, i) => strandB[i] !== curr && acc + 1, 0);
 };
+//C,A,G and T
