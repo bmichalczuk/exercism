0c081b0a82de43aef357d05b44165b1898d10a70
bmichalczuk <barmic90@hotmail.com>
2020-04-11 23:37:25 +0200

    submit triangle.js

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -3,20 +3,27 @@
 // convenience to get you started writing code faster.
 //
 
+
+const triangleInequality = (a,b,c) => (a + b) > c && (a + c) > b && (b + c) > a;
+
 export class Triangle {
-  constructor() {
-    throw new Error("Remove this statement and implement this function");
+  constructor(a,b,c) {
+    this.sides = [a, b, c];
+    this.isTriangle = triangleInequality(a,b,c) && [a, b, c].filter(sideLength => sideLength > 0).length > 0;
   }
 
   isEquilateral() {
-    throw new Error("Remove this statement and implement this function");
+    const {isTriangle, sides} = this;
+    return isTriangle && new Set(sides).size === 1;
   }
 
   isIsosceles() {
-    throw new Error("Remove this statement and implement this function");
+    const {isTriangle, sides} = this;
+    return isTriangle && new Set(sides).size <= 2;
   }
 
   isScalene() {
-    throw new Error("Remove this statement and implement this function");
+    const {isTriangle, sides} = this;
+    return isTriangle && sides.length === new Set(sides).size;
   }
 }
