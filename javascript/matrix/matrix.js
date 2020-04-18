3f2619778b1e81651891f514d8d0b2daa6113ebf
bmichalczuk <barmic90@hotmail.com>
2020-04-18 15:17:51 +0200

    download side exercises

--- javascript/matrix/matrix.js
+++ javascript/matrix/matrix.js
@@ -3,20 +3,35 @@
 // convenience to get you started writing code faster.
 //
 
-export class Matrix {
+ export class Matrix {
   constructor(raw) {
     this.raw = raw;
+    this.cashe = {
+      rows: null,
+      columns: null
+    };
+  }
+  parseRaw() {
+    return this.raw.split("\n");
   }
   getColumn(col) {
     return this.rows.map(row => row[col]);
   }
+  getRow(rawRow) {
+    return rawRow.split(" ").map(str => Number(str));
+  }
   get rows() {
-    return this.raw
-      .split("\n")
-      .map(row => row.split(" "))
-      .map(arr => arr.map(str => Number(str)));
+    if(this.cashe.rows) {
+      return this.cashe.rows;
+    } 
+    this.cashe.rows = this.parseRaw().map(this.getRow);
+    return this.cashe.rows;
   }
   get columns() {
-    return this.rows[0].map((el, i) => this.getColumn(i));
+    if(this.cashe.columns) {
+      return this.cashe.columns;
+    } 
+    this.cashe.columns = this.rows[0].map((el, i) => this.getColumn(i));
+    return this.cashe.columns;
   }
 }
